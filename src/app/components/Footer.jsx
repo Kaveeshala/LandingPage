79c9408bb9d454e1eac2f927bdd08a7e3e59e75d
kaveeshala <it22110916@my.sliit.lk>
2025-03-05 17:25:04 +0530

    create footer section

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -1,40 +1,70 @@
+import { Input } from "@/components/ui/input"; 
+import { Button } from "@/components/ui/button"; 
+import { Label } from "@/components/ui/label";
+import Link from 'next/link';
+
 const Footer = () => {
+
+  const navLinks = [
+    {
+      name: 'Home',
+      link: '/'
+    },
+    {
+      name: 'Buy',
+      link: '/'
+    },
+    {
+      name: 'About',
+      link: '/'
+    },
+    {
+      name: 'Contact us',
+      link: '/'
+    },
+  ]
+  
   return (
     <footer>
-      <hr className='my-3 w-full mx-auto border-gray-300' />
-      
-      <div className="flex justify-between">
-        <div className="ml-25">
-          <h2 className="mt-6 text-black font-bold text-6xl">Tilestone</h2>
-          <p className="mt-6 text-gray-400">Discover Your Dream Home - Explore Listings <br /> & Find Perfect Match!</p>
+      <hr className="w-full mx-auto border-gray-300 p-4"/>
+      <div className="max-w-7xl mx-auto flex flex-col">
+      <div className='flex justify-between items-center'>
+        <div className="flex flex-col gap-6">
+          <h1 className="text-6xl font-semibold tracking-wide">Tilestone</h1>
+          <p className=" text-gray-400">Discover Your Dream Home - Explore Listings <br /> & Find Perfect Match!</p>
 
-          <h1 className="mt-12 text-sm font-semibold">Subscribe to our newsletter</h1>
+          <Label>Subscribe to our newsletter</Label>
 
-          {/* Email Input */}
-          <div className="flex-colum mt-2 mb-9 flex justify-between">
-            <div className="flex items-center space-x-2">
-              <input
+          <div className="flex flex-cols items-center gap-2">
+          <Input
                 type="email"
                 id="email"
-                className="w-64 h-9 p-2 border border-gray-300 rounded"
                 placeholder="Email"
+                className="w-64 h-9  border border-gray-300"
               />
-              <button className="h-9 w-17 py-1 px-2 bg-black text-white rounded">Send</button>
-            </div>
+              <Button className='transition cursor-pointer active:scale-90'>send</Button>
           </div>
         </div>
-        <ul className="flex flex-col space-y-6 space-x-36 pt-6 justify-start text-gray-500 ">
-          <li>Home</li>
-          <li>Buy</li>
-          <li>About</li>
-          <li>Contact us</li>
-        </ul>
+
+        <div className="flex flex-cols justify-between">
+          <div className="flex flex-col gap-6 text-gray-400 space-x-10"> 
+          {navLinks.map((link, index) => (
+              <Link href={link.link} key={index}>{link.name}</Link>
+
+            ))}
+          </div>
+        </div>
+
+      </div>
       </div>
 
-      <hr className='my-3 ml-25 mr-25 mx-auto border-gray-300' />
+      <div className="flex flex-col gap-2">
+      <hr className=" mt-18 ml-25 mr-25 mx-auto border-gray-300" />
       <p className="mt-6 mb-6 text-center text-sm tracking-wide text-gray-500">
-            &copy;2025 Tilestone. All rights Reserved.
-        </p>
+        &copy;2025 Tilestone. All rights Reserved.
+      </p>
+      </div>
+      
     </footer>
   );
 };
