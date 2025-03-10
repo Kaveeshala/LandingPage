e7478897b25023de97861364ede9c8e5b23e55b7
kaveeshala <it22110916@my.sliit.lk>
2025-03-10 20:40:47 +0530

    modify the article section

--- src/app/components/Artcles.jsx
+++ src/app/components/Artcles.jsx
@@ -3,10 +3,10 @@ import { Marquee } from "@/components/magicui/marquee"
 export default function Artcles() {
   return (
     <div>
-      <h1 className="text-center font-semibold text-6xl py-8">Articles</h1> {/* Increased text size */}
+      <h1 className="text-center font-semibold text-6xl mt-24">Articles</h1> {/* Increased text size */}
       <div className="py-8"></div> {/* Added space */}
       
-      <Marquee className="your-custom-class" pauseOnHover speed={50}>
+      <Marquee className="your-custom-class mt-8" pauseOnHover speed={50}>
         <div className="flex items-center justify-center space-x-4">
           <span className="text-5xl">•</span> {/* Increased text size */}
           <span className="text-5xl">Your Bathroom</span> {/* Increased text size */}
@@ -19,7 +19,7 @@ export default function Artcles() {
       
       <div className="py-4"></div> {/* Added space between marquees */}
       
-      <Marquee className="your-custom-class" pauseOnHover speed={30}>
+      <Marquee className="your-custom-class mt-8" pauseOnHover speed={30}>
         <div className="flex items-center justify-center space-x-4">
           <span className="text-5xl">•</span> {/* Increased text size */}
           <span className="text-5xl">Bathroom Trends You'll Want To Incorporate in Your Bathroom</span> {/* Increased text size */}
@@ -32,7 +32,7 @@ export default function Artcles() {
       
       <div className="py-4"></div> {/* Added space between marquees */}
       
-      <Marquee className="your-custom-class" pauseOnHover speed={20}>
+      <Marquee className="your-custom-class mt-8" pauseOnHover speed={20}>
         <div className="flex items-center justify-center space-x-4">
           <span className="text-5xl">•</span> {/* Increased text size */}
           <span className="text-gray-400 text-5xl">July 03, 2025</span> {/* Increased text size */}
